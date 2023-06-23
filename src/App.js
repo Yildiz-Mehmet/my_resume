aa79a5bba9cff39b31877cb036dcdfc371130397
yıldız <mesel4461@gmail.com>
2023-06-23 19:12:00 +0300

    as

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Skills from "./components/pages/skills/Skills";
+import Experiences from "./components/pages/experiences/Experiences";
+import Educations from "./components/pages/educations/Educations";
+import Portfolios from "./components/pages/portfolios/Portfolios";
 
 function App() {
   return (
     <Router>
-      <Route />
+      <Route path="/" element={<Home />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/experiences" element={<Experiences />} />
+      <Route path="/educations" element={<Educations />} />
+      <Route path="/portfolios" element={<Portfolios />} />
     </Router>
   );
 }
