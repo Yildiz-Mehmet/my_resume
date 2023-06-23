387f42c5697746b6066d72d45f1002448cb28204
yıldız <mesel4461@gmail.com>
2023-06-23 03:55:25 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "materialize-css/dis/css/materialize.min.css";
+import "materialize-css/dis/js/materialize.min.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome the resume website</h1>
+    </div>
+  );
 }
 
 export default App;
